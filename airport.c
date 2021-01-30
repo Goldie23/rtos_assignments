4e6e660dcfd68f61c34bd3e74381ba5053b17a53
U-DESKTOP-4N28QED\Matthew <ms5570@mun.ca>
2021-01-29 22:36:29 -0330

    s

--- airport.c
+++ airport.c
@@ -71,7 +71,7 @@
 
             if(validStop){
                 port->flight_size++;
-                port = realloc(port, sizeof(struct airport) + port->flight_size);
+            //    port = realloc(port, sizeof(struct airport) + port->flight_size);
 //                port->flights[port->flight_size] = new_flight;
 
 
@@ -94,7 +94,7 @@
   *                  size required.
   */
  void        ap_flights(struct airport *ap, struct flight **fpp, size_t *n){
-/*
+
                 if(*n < ap->flight_size){
                     *n = ap->flight_size;
                 }
@@ -114,7 +114,7 @@
 
 
 
-*/
+
  }
 
  /** Increment a airport's refcount. */
