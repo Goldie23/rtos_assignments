080021022871291c463c7f604ab203595753d430
U-DESKTOP-4N28QED\Matthew <ms5570@mun.ca>
2021-01-29 23:07:30 -0330

    s

--- airport.c
+++ airport.c
@@ -65,7 +65,7 @@
                 printf("\n %d", new_flight->f_stop_count);
 
                 for(int i = 0; i < new_flight->f_stop_count; i++){
-                        printf("\n %c", (*(new_flight->f_stops))->icao_code);
+                        printf("\n %s", (*(new_flight->f_stops))->icao_code);
              //           validStop = true;
                //         printf("flight was found!\n");
                 }
