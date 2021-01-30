fea059f3fbbed5bf0045fdf76f144355e9232cf1
U-DESKTOP-4N28QED\Matthew <ms5570@mun.ca>
2021-01-29 21:19:42 -0330

    s

--- flight.c
+++ flight.c
@@ -79,7 +79,8 @@ void		flight_free(struct flight* this_flight){
 void		flight_change_equipment(struct flight* this_flight, struct aircraft* craft){
 
                
-                if(craft == NULL){
+                if(craft == NULL && this_flight->f_aircraft != NULL){
+                    ac_release(this_flight->f_aircraft);
                     this_flight ->f_aircraft = NULL;
                     return;
                 }
