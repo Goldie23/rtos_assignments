d1b076f9ebd71bb2171ca44429043718d64a99b1
U-DESKTOP-4N28QED\Matthew <ms5570@mun.ca>
2021-01-29 22:56:58 -0330

    s

--- airport.c
+++ airport.c
@@ -65,12 +65,12 @@
                 printf("\n %d", new_flight->f_stop_count);
 
                 for(int i = 0; i < new_flight->f_stop_count; i++){
-                    if(*(new_flight->f_stops+i) == port)
+                    if(ap_code(*(new_flight->f_stops+i)) == ap_code(port) )
                         validStop = true;
 
                 }
 
-
+                
             if(validStop){
                 port->flight_size++;
                 port = realloc(port, sizeof(struct airport) + port->flight_size);
