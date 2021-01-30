55a8d3fcc10dbe1ee0ef2e63a87fd7d44db9c168
U-DESKTOP-4N28QED\Matthew <ms5570@mun.ca>
2021-01-29 21:37:32 -0330

    s

--- flight.c
+++ flight.c
@@ -51,7 +51,7 @@ struct flight*	flight_create(const char *airline, uint16_t number,
 
                 new_flight->f_number = number;
                 new_flight->f_stops = original_addr - i;
-
+                printf("this made it to the end");
                 return new_flight;
 }
 
